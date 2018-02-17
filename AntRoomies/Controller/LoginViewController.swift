0f712a18e39428353ed62273559a4c76269ff5a9
Winston Lan <winston.chunhua.lan@gmail.com>
2018-02-17 11:54:07 -0800

    Implemented UI for Login View
    Implemented MVP version of login view UI.

---
+++ AntRoomies/Controller/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  AntRoomies
+//
+//  Created by Winston Lan on 2/17/18.
+//  Copyright © 2018 winstonlan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
